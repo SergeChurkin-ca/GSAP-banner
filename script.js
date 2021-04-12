6b6a52ea622f4ed71d2275c340ecd48c85777a16
SergeChurkin-ca <sergechurkin@yahoo.com>
2021-04-12 16:33:18 -0400

    close and open popup

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const closePopUpButton = document.querySelector(".btn-close");
-
+const openPopUpButton = document.querySelector(".btn-legal");
 
 closePopUpButton.addEventListener("click", popUpClose);
+openPopUpButton.addEventListener("click", popUpOpen);
 
 function popUpClose() {
     document.querySelector('.legal-popup').style.cssText = "display: none"
+    document.querySelector('.banner-wrapper').style.cssText = "display: block; transition: 1s ease"
+}
+
+function popUpOpen() {
+    document.querySelector('.legal-popup').style.cssText = "display: block";
+    document.querySelector('.banner-wrapper').style.cssText = "display: none"
 }
