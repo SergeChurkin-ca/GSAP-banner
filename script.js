48d8c8e0dfe35cca530ea7a8f1c747fd087b157f
SergeChurkin-ca <sergechurkin@yahoo.com>
2021-04-13 00:12:58 -0400

    fixed justify text

--- script.js
+++ script.js
@@ -17,9 +17,14 @@ function popUpOpen() {
 }
 
 // chevrons animation
-
-const tl = gsap.timeline({ repeat: -1 });
 /*
-tl.from(".chevrons", { duration: 1.5, x: -90, opacity: 0.3, ease: 'power2' })
-    .to('.slide-1', { duration: 1.5, opacity: 0, delay: 1 }, "+=1")
-    .to('.chevrons', { duration: 1, x: 0, opacity: 1 }, 1)
+const tl = gsap.timeline({ repeat: -1 });
+
+
+tl.from(".chevrons", { duration: 1, x: -90, opacity: 0.3, ease: 'power2' })
+    // .to('.slide-1', { duration: 1, opacity: 0, delay: 1 }, 1)
+    .set('.slide-2', { duration: 1, opacity: 0, delay: 1, ease: 'power2' })
+    .to('.slide-2', { duration: 2, opacity: 1, delay: 1, ease: 'power2' })
+    .set('.slide-2', { duration: 1, opacity: 0, ease: 'power2' })
+
+.to('.chevrons', { duration: 1, x: 0, opacity: 1 }, 5)
