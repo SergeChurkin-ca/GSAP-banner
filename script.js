a58b0f4b3d3ac8cc8f0d81844b3017d581761ec0
SergeChurkin-ca <sergechurkin@yahoo.com>
2021-04-13 15:46:02 -0400

    mvp complete

--- script.js
+++ script.js
@@ -15,16 +15,29 @@ function popUpOpen() {
     document.querySelector('.legal-popup').style.cssText = "display: block";
     document.querySelector('.banner-wrapper').style.cssText = "display: none"
 }
-/*
+
 // chevrons animation
 
 const tl = gsap.timeline({ repeat: -1 });
 
 
 tl.from(".chevrons", { duration: 1, x: -90, opacity: 0.3, ease: 'power2' })
-    // .to('.slide-1', { duration: 1, opacity: 0, delay: 1 }, 1)
-    .set('.slide-2', { duration: 1, opacity: 0, delay: 1, ease: 'power2' })
-    .to('.slide-2', { duration: 2, opacity: 1, delay: 1, ease: 'power2' })
-    .set('.slide-2', { duration: 1, opacity: 0, ease: 'power2' })
+    .to('.slide-1', { duration: 1, opacity: 0, delay: 1 }, 1)
+
+.to('.slide-2', { duration: 1, opacity: 1, delay: 1, ease: 'power2' })
+    .to('.slide-2', { duration: 1, opacity: 0, delay: 1, ease: 'power2' })
+
+.to('.slide-3', { duration: 1, opacity: 1, delay: 1, ease: 'power2' })
+    .to('.slide-3', { duration: 1, opacity: 0, delay: 1, ease: 'power2' })
+
+.to('.slide-4', { duration: 1, opacity: 1, delay: 1, ease: 'power2' })
+    .to('.slide-4', { duration: 1, opacity: 0, delay: 1, ease: 'power2' })
+
+.to('.slide-5', { duration: 1.5, opacity: 1, delay: 1, ease: 'power3' })
+    .to('.slide-5', { duration: 1, opacity: 0, scale: 0.1, delay: 1, ease: 'power3' })
+
+
+
+
 
 .to('.chevrons', { duration: 1, x: 0, opacity: 1 }, 5)
