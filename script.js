12b605674b061fbc9729196080bf94d4b218b1de
SergeChurkin-ca <sergechurkin@yahoo.com>
2021-04-12 23:00:17 -0400

    slide 2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// legap popup
+
 const closePopUpButton = document.querySelector(".btn-close");
 const openPopUpButton = document.querySelector(".btn-legal");
 
@@ -13,3 +15,11 @@ function popUpOpen() {
     document.querySelector('.legal-popup').style.cssText = "display: block";
     document.querySelector('.banner-wrapper').style.cssText = "display: none"
 }
+
+// chevrons animation
+
+const tl = gsap.timeline({ repeat: -1 });
+/*
+tl.from(".chevrons", { duration: 1.5, x: -90, opacity: 0.3, ease: 'power2' })
+    .to('.slide-1', { duration: 1.5, opacity: 0, delay: 1 }, "+=1")
+    .to('.chevrons', { duration: 1, x: 0, opacity: 1 }, 1)
